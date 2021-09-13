c88031df5096b16c0f61089a5a745e01189f0abe
jeo19 <terrakkk@outlook.com>
2021-09-13 08:40:54 +0800

    feat:Render the Route in App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
-function App() {
-  return <div>Hello React</div>;
-}
+import React from "react";
+import { Route } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
+
+const App = () => {
+  return (
+    <div>
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/About" component={About}></Route>
+    </div>
+  );
+};
 
 export default App;
