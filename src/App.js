51e203baa206ff2e4d6e0d27a743185254963467
jeo19 <terrakkk@outlook.com>
2021-09-14 09:26:18 +0800

    feat:Add the Link in App component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Link } from "react-router-dom";
 import Home from "./Home";
 import About from "./About";
 
 const App = () => {
   return (
     <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/About">About</Link>
+        </li>
+      </ul>
       <Route path="/" exact={true} component={Home}></Route>
       <Route path="/About" component={About}></Route>
     </div>
